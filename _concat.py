2ac856f600c6fd23bdbd642c2b8ecd6130aadb9a
ghostrider101218 <142645808+ghostrider101218@users.noreply.github.com>
2025-09-16 12:44:16 -0500

    chore: script for .md files concatenation

---
+++ _concat.py
@@ -0,0 +1,21 @@
+import glob
+
+report_path = "Report.md"
+export_path = "export/Report.md"
+chapter_path = "[0-9]*.md"
+
+files = sorted(glob.glob(chapter_path))
+
+with open(export_path, "w", encoding="utf-8") as outfile:
+    for i,filename in enumerate(files):
+        with open(filename, "r", encoding="utf-8") as infile:
+            outfile.write(infile.read())
+            outfile.write("\n\n---\n\n")
+            if i < len(files) - 1:
+                outfile.write("\n\n<div style=\"page-break-after: always;\"></div>\n\n")
+
+    
+with open(report_path, "w", encoding="utf-8") as outfile:
+    with open(export_path, "r", encoding="utf-8") as infile:
+        outfile.write(infile.read())
+        outfile.write("\n\n---\n\n")
